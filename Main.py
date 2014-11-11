bc6d0adcde895503e009ee5c9bd29d4b84f135df
Johnidel <johnidel@aol.com>
2014-11-11 10:39:50 -0500

    Added Stuff

--- Main.py
+++ Main.py
@@ -13,21 +13,53 @@ TIME_PER_FRAME = 1.0 / 30.0
 time_start = 0
 timeS = time.time()
 playing = True
+keyPressedW = False
+keyPressedA = False
+keyPressedS = False
+keyPressedD = False
 
 #Main Game Loop
 while playing == True:
 	#get the time at start of this specific cycle of loop
 	time_start = time.time()
-
+	deltaX = 0
+	deltaY =0
 	#check for key and mouse events
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 		if event.type == pygame.KEYDOWN:
 			print("BLAHHHH")
-			if event.key == pygame.K_w:
+			if event.key == pygame.K_ESCAPE:
 				playing = False
-            
+			if event.key == pygame.K_w:
+				keyPressedW = True
+			if event.key == pygame.K_s:
+				keyPressedS = True
+			if event.key == pygame.K_a:
+				keyPressedA = True
+			if event.key == pygame.K_d:
+				keyPressedD = True
+		if event.type == pygame.KEYUP:
+			print("BLAHHHH")
+			if event.key == pygame.K_w:
+				keyPressedW = False
+			if event.key == pygame.K_s:
+				keyPressedS = False
+			if event.key == pygame.K_a:
+				keyPressedA = False
+			if event.key == pygame.K_d:
+				keyPressedD = False
+
+	if keyPressedW:
+		deltaY = -5
+	if keyPressedA:
+		deltaX = -5
+	if keyPressedS:
+		deltaY = 5
+	if keyPressedD:
+		deltaX = 5
+
 	num += 1
 
 	#make screen black(erase screen)
@@ -42,8 +74,10 @@ while playing == True:
 	#draw text on surface
 	rend = font.render(string, 1, (255,255,255))
 
+	rect = rect.move(deltaX, deltaY)
 	#daw surface on screen
 	screen.blit(rend, (0, 400))
+	pygame.draw.rect(screen, (255,255,255), rect)
 
 	#update display
 	pygame.display.flip()
