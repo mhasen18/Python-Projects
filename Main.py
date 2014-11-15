c3a571782786a671c91b6c8b38d8904b7828c966
Johnidel <johnidel@aol.com>
2014-11-15 04:19:51 -0500

    Guard added

--- Main.py
+++ Main.py
@@ -8,7 +8,17 @@ import urllib.request
 import io
 from PIL import Image
 
-size = (640, 480)
+'''
+MESSAGE TO TEAMMATES:
+Alright so this should be enough to get us started. I got a lot
+of the annoying math shit out of the way for rotation and the
+implementation of new images should require minimal change
+So you guys go ahead and play around with stuff and text/email
+me if you have any ideas/problems/concerns or just need some debugging.
+Sky's the limit
+		- JOHN
+'''
+size = (1024, 768)
 xCenter, yCenter= size[0] / 2, size[1] / 2
 
 #initialize window
@@ -25,44 +35,24 @@ keyPressedA = False
 keyPressedS = False
 keyPressedD = False
 
-
-
-'''
 guards = []
-guards.append()
 
-class guard():
-	path
-	loc
-	x, y
-	def __init__(self):
-	def update(self):
-		move, do i see a body, 
-		if collision:
-	def draw(pygame.display):
-	def getRect()
-
-
-
-def update():
-	for guard in guards:
-		guard.update()
-	checkColiisons()
-	pass
-def draw():
-	for guard in guards:
-		guard.draw()
-	for wall in walls:
-		wall.draw()
-
-	dooors.draw()
-	pass
-
-'''
 def draw(walls):
 	for wall in walls:
 		pygame.draw.rect(screen,(104,104,104),wall)
 
+def rotPoint(point, axis, ang):
+	""" Orbit. calcs the new loc for a point that rotates a given num of degrees around an axis point,
+	+clockwise, -anticlockwise -> tuple x,y
+	"""
+	x, y = point[0] - axis[0], point[1] - axis[1]
+	radius = math.sqrt(x*x + y*y) # get the distance between points
+
+	RAng = math.radians(ang)
+	h = axis[0] + ( radius * math.sin(RAng) )
+	v = axis[1] + ( radius * math.cos(RAng) )
+
+	return h, v
 
 pygame.event.set_grab(True)
 pygame.mouse.set_visible(False)
@@ -72,10 +62,18 @@ if not fileExists:
 	URL = "http://i.imgur.com/14GOa9C.png"
 	urllib.request.urlretrieve(URL, "player.png")
 
+fileExists = os.path.isfile("flashlight.png")
+if not fileExists:
+	URL = "http://i.imgur.com/M9LEv1q.png"
+	urllib.request.urlretrieve(URL, "flashlight.png")
+
 #load image
 playerImg = pygame.image.load("player.png")
+flashlight = pygame.image.load("flashlight.png")
+
 #move image to center of screen
 playerRect= playerImg.get_rect().move(320, 240)
+
 #set mouse coord right above image
 pygame.mouse.set_pos(playerRect.center[0], playerRect.center[1] - 40)
 
@@ -87,14 +85,97 @@ def rot_center(image, rect, angle):
 	return rot_image,rot_rect
 
 
+class Guard:
+	def __init__(self, imageFile, pos_, path_):
+		#initialize vars
+		self.path = path_
+		self.pos = [0,0]
+		self.pos[0], self.pos[1] = pos_[0], pos_[1]
+		self.img = pygame.image.load(imageFile)
+		self.flash = pygame.image.load("flashlight.png")
+		self.guardRect = self.img.get_rect()
+		self.guardRect.x = self.pos[0]
+		self.guardRect.y = self.pos[1]
+		#double size of flashlight image
+		self.flash = pygame.transform.scale2x(self.flash)
+
+	def draw(self, screen):
+		#rotate the image of the guard
+		rot_img, self.guardRect = rot_center(self.img, self.guardRect, self.theta)
+
+		#move pos based on speed
+		self.pos[0], self.pos[1] = self.pos[0] - (self.magMove[0] * 5), self.pos[1] - (self.magMove[1] * 5)
+
+		#move the guard based on position(I dont know why I am using the flashlight rect but it works for some reason)
+		self.guardRect.center = self.pos[0] + self.guardRect.width / 2 + ((self.flash.get_width() - self.guardRect.width) / 2) , self.pos[1] + self.guardRect.height / 2 + ((self.flash.get_height() - self.guardRect.height) / 2)
+		
+
+		#draw guard on screen
+		screen.blit(rot_img, self.guardRect)
+
+		#rotate flashlight
+		rot_img, newRect = rot_center(self.flash, self.flash.get_rect(), self.theta)
+
+		#position flashlight at center of guard
+		newRect.center = self.guardRect.center
+
+		#move the flashlight to tip of guard based on direction(this was hard)
+		newRect.center = (newRect.center[0] + 1 - (math.sin(math.radians(self.theta)) * ( 44 + self.flash.get_height() / 2)), newRect.center[1] + 1 - (math.cos(math.radians(self.theta)) * (44 + self.flash.get_height() / 2)))
+
+		#draw flashlight
+		screen.blit(rot_img, newRect)
+
+
+	def update(self):
+		self.magMove = [0, 0]
+		thet = 0
+
+		#get direction of movement based on path
+		dirMove = (self.guardRect.center[0] - self.path[0][0], self.guardRect.center[1] - self.path[0][1])
+
+		#get unit vector for movement
+		mag = ((dirMove[0] ** 2) + (dirMove[1] ** 2)) ** (1/2)
+
+		#set the class var to the movement determine angle
+		if mag != 0: 
+			self.magMove[0], self.magMove[1] = dirMove[0] / mag, dirMove[1] / mag
+			thet = math.asin(dirMove[0]/ mag)
+
+		#adjust angle based of the direction of movement
+		if dirMove[1] > 0 and dirMove[0] < 0:
+			thet = math.pi - thet
+		if dirMove[1] > 0 and dirMove[0] >= 0:
+			thet = math.pi - thet
+
+		#convert to degrees and assign to class var
+		self.theta = -thet * 180 / math.pi + 180
+
+		#if the guard is at the current position: take the next path location and put the current at the end of stack
+		if math.fabs(self.path[0][0] - self.guardRect.center[0]) <= 5 and math.fabs(self.path[0][1] - self.guardRect.center[1]) <= 5:
+			tmp = self.path.pop(0)
+			self.path.append(tmp)
+		
+
+#initialize guard
+path1 =  [(80,60),(460, 60), (460,160),(80, 160)]
+path2 = [(200, 600), [800, 600]]
+path3 = [(60, 700), (980, 700), (980, 60), (60,60)]
+path4 = [(700, 300), (900, 300), (800, 500)]
+
+guards.append(Guard("player.png", [300,200], path1))
+guards.append(Guard("player.png", [100,200], path2))
+guards.append(Guard("player.png", [300,200], path3))
+guards.append(Guard("player.png", [300,200], path4))
+
 #Main Game Loop
 while playing == True:
-
 	#get the time at start of this specific cycle of loop
 	time_start = time.time()
 
 	deltaF = 0
 	deltaS =0
+	for ele in guards:
+		ele.update()
 
 	#check for key and mouse events
 	#Polling input
@@ -154,7 +235,7 @@ while playing == True:
 
 
 	#make screen black(erase screen)
-	screen.fill((255,255,255))
+	screen.fill((0,0,255))
 
 	string = 'USE WASD TO MOVE AND MOUSE TO "STEER"'
 
@@ -175,15 +256,15 @@ while playing == True:
 	playerRect = playerRect.move(dirMovement[0] * deltaF, dirMovement[1] * deltaF)
 	playerRect = playerRect.move(-dirMovement[1] * deltaS, dirMovement[0] * deltaS)
 
-	#draw bounding box of img
-	pygame.draw.rect(screen, (0,0,0), playerRect, 1)
-
 	#draw rotated img
 	collisionRect = pygame.Rect(playerRect.center[0] - 32, playerRect.center[1] - 32, 64, 64)
 	screen.blit(rot_image, playerRect)
 	rectForWalls = [pygame.Rect((120,180,80,100)), pygame.Rect((5,60,10,20))]
 	draw(rectForWalls)
 	
+	for ele in guards:
+		ele.draw(screen)
+
 	for rectangle in rectForWalls:
 		if rectangle.colliderect(collisionRect):
 			playerRect = playerRect.move(dirMovement[0] * -deltaF, dirMovement[1] * -deltaF)
